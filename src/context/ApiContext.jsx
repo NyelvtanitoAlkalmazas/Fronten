149fd44a7e292c2fc6ad3a2821164498c5c7175d
lajkogege <lajkogege@gmail.com>
2024-11-19 11:08:40 +0100

    providerApi

---
+++ src/context/ApiContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useState } from "react";
+import { fetchData } from "../http";
+
+
+export const ApiContext = createContext("");
+
+export const ApiProvider = ({ children }) => {
+    const [sentences, setSentences] = useState([]);
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchSentences = async () => {
+            try {
+                const data = await fetchData("/sentences")
+                setSentences(data)
+            }
+            catch (error) {
+                console.error(error)
+            }
+        }
+
+        const fetchTasks = async () => {
+            try {
+                const data = await fetchData("/tasks");
+                setTasks(data)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchTasks();
+        fetchSentences();
+
+    }, []);
+
+    return (
+        <ApiContext.Provider value={{ sentences, tasks }}>
+            {children}
+        </ApiContext.Provider>
+    );
+
+}
