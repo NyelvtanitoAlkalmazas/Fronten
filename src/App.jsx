47ebb2ac468d426aff145154a55df06e3e1747c9
KAkos27 <akoskosztolanyi@gmail.com>
2024-11-19 11:18:16 +0100

    app componens

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,27 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
+
 import "./App.css";
-import { fetchData } from "./http";
+import Choose from "./components/Choose";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedPage, setSelectedPage] = useState(0);
 
-  useEffect(() => {
-    const fetchTasks = async () => {
-      try {
-        const data = await fetchData("/tasks");
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchTasks();
-  }, []);
+  const handleClickLink = (pageId) => {
+    setSelectedPage(pageId);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+      <a onClick={() => handleClickLink(1)}>Válaszd ki!</a>
+      <a onClick={() => handleClickLink(2)}>Egészítsd ki!</a>
+      <a onClick={() => handleClickLink(3)}>Rakd sorba!</a>
+    </div>
+
+    {
+      if (selectedPage == 1) {
+        <Choose/>
+      }
+    }
   );
 }
 
--- src/context/ApiContext.jsx
+++ src/context/ApiContext.jsx
@@ -1,42 +1,38 @@
 import { createContext, useEffect, useState } from "react";
 import { fetchData } from "../http";
 
-
 export const ApiContext = createContext("");
 
 export const ApiProvider = ({ children }) => {
-    const [sentences, setSentences] = useState([]);
-    const [tasks, setTasks] = useState([]);
+  const [sentences, setSentences] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
-    useEffect(() => {
-        const fetchSentences = async () => {
-            try {
-                const data = await fetchData("/sentences")
-                setSentences(data)
-            }
-            catch (error) {
-                console.error(error)
-            }
-        }
+  useEffect(() => {
+    const fetchSentences = async () => {
+      try {
+        const data = await fetchData("/sentences");
+        setSentences(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        const fetchTasks = async () => {
-            try {
-                const data = await fetchData("/tasks");
-                setTasks(data)
-            } catch (error) {
-                console.error(error);
-            }
-        };
+    const fetchTasks = async () => {
+      try {
+        const data = await fetchData("/tasks");
+        setTasks(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-        fetchTasks();
-        fetchSentences();
+    fetchTasks();
+    fetchSentences();
+  }, []);
 
-    }, []);
-
-    return (
-        <ApiContext.Provider value={{ sentences, tasks }}>
-            {children}
-        </ApiContext.Provider>
-    );
-
-}
+  return (
+    <ApiContext.Provider value={{ sentences, tasks }}>
+      {children}
+    </ApiContext.Provider>
+  );
+};
