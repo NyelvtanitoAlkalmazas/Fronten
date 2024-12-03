c9ff573d7d712a53c11f90099e53456907f317f7
KAkos27 <akoskosztolanyi@gmail.com>
2024-12-03 08:57:38 +0100

    kis jav

--- src/components/Complete.jsx
+++ src/components/Complete.jsx
@@ -2,8 +2,6 @@ import { useContext, useState } from "react";
 import { ApiContext } from "../context/api-context-provider";
 
 const Complete = () => {
-
-
   const { sentences, tasks } = useContext(ApiContext);
   const [currentTaskId, setCurrentTasksId] = useState(0);
 
@@ -11,9 +9,9 @@ const Complete = () => {
 
   const handleInput = (event) => {
     if (event.target.value == tasks[currentTaskId].option_one) {
-      setIsCorrect(true)
+      setIsCorrect(true);
     }
-  }
+  };
 
   const nextSentences = () => {
     setCurrentTasksId((prevId) => prevId + 1); // Előző ID-hoz hozzáad 1-et
@@ -23,18 +21,15 @@ const Complete = () => {
     <div>
       {sentences.length > 0 && ( //if elág
         <>
-          <h2>{sentences[0].sentence}</h2>
-          <h3>Gépeld be a megfelelő  alakot!</h3>
+          <h2>{sentences[1].sentence}</h2>
+          <h3>Gépeld be a megfelelő alakot!</h3>
 
           <form>
             <label>
               <p>{tasks.length > 0 && tasks[currentTaskId].sentence}</p>
               <p>({tasks.length > 0 && tasks[currentTaskId].basis})</p>
-              <input
-                type="text"
-                onChange={handleInput}
-              />
-              <p>{isCorrect ? "Jó" : "Rossz"}</p>
+              <input type="text" onChange={handleInput} />
+              <p>{isCorrect ? "✅" : "❌"}</p>
             </label>
           </form>
 
